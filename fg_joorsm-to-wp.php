e16846fa74c771c26a7fe04b2ef4ab12dcd2274a
Jean-Pierre MENA <pythonrubylang@gmail.com>
2014-05-09 17:33:37 +0200

    formatting the code the same way as the importer it extends

--- fg_joorsm-to-wp.php
+++ fg_joorsm-to-wp.php
@@ -15,19 +15,31 @@ if ( !defined('WP_LOAD_IMPORTERS') )
 	return;
 
 if ( class_exists('fgj2wp', false) ) {
-	add_filter('fgj2wp_get_categories', gerer_categories);
-	add_action('fgj2wp_post_insert_post', supprimer_post_non_montreuil);
-}
-
-function gerer_categories($tab_categories){
-	$tab_filtree = array();
-	foreach ($tab_categories as $cat){
-		$titre = $cat["title"];
-		if(preg_match ( '/^rs\-montreuil:/' ,$titre)){
-			$tab_filtree[] = $cat;
+	
+	if ( !class_exists('JooRsm', false) ) {
+		class JooRsm extends fgj2wp {
+			public function __construct(){
+				add_filter('fgj2wp_get_categories', array(&$this, 'gerer_categories'));
+				//add_action('fgj2wp_post_insert_post', array(&$this, 'supprimer_post_non_montreuil'));
+			}
+			function gerer_categories($tab_categories){
+				$tab_filtree = array();
+				foreach ($tab_categories as $cat){
+					$titre = $cat["title"];
+					if(preg_match ( '/^rs\-montreuil:/' ,$titre)){
+						$tab_filtree[] = $cat;
+					}
+				}
+				return $tab_filtree;
+			}
 		}
 	}
-	return $tab_filtree;
 }
+if ( !function_exists( 'joorsmwp_load' ) ) {
+	add_action( 'plugins_loaded', 'joorsmwp_load', 20 );
 
+	function joorsmwp_load() {
+		$joo_rsm_to_wp = new JooRsm();
+	}
+}
 ?>
